acc21b74abed110eac244996e3ef9c493f36591a
Belkysp15 <77577845+Belkysp15@users.noreply.github.com>
2021-10-02 10:39:48 -0400

    Update nominabp.py

--- nominabp.py
+++ nominabp.py
@@ -2,16 +2,16 @@ import os
 import sqlite3 
 
 array7 = []
-emp = []
+bono1 = 0
+bono_general1 = 0
 cedula = 0
-fecha_ingreso = 0
+emp = []
 empleado = 0
-salario_base = 0
-salario_base2 = 0
+fecha_ingreso = 0
 horas_extras = 0
 salario = 0
-bono1 = 0
-bono_general = 0
+salario_base = 0
+salario_base2 = 0
 
 archivo2=open("C:/Users/BP/Documents/python nomina/pago_nomina_12sep2021.py","w")
 archivo2.write("Cedula****Fecha Ingreso******Empleado***********************Sueldo Base*****Horas Extras*****Monto a Pagar\n")
@@ -19,7 +19,6 @@ archivo2.write("Cedula****Fecha Ingreso******Empleado***********************Suel
 from colorama import Fore, init
 init()
 print(Fore.GREEN + "NOMINA DE LA EMPRESA YABADABADU")
-
 
 from script_pago_nomina import *
 
@@ -32,13 +31,11 @@ with open('C:/Users/BP/Documents/python nomina/nomina.txt','r') as archivo:
         print(Fore.GREEN + "RESUMEN DE NOMINA")
         linea = archivo.readline()
         array7 = linea
-
         array = []
         array = array7.split( )
 
         cedula = array[0].lower().replace(' ', '')
         print("Cedula: ",cedula)
-
         fecha_ingreso = array[1].lower().replace(' ', '')
         fecha_f = []
         fecha_f = fecha_ingreso.split('/')
@@ -46,32 +43,31 @@ with open('C:/Users/BP/Documents/python nomina/nomina.txt','r') as archivo:
         ff = fecha_f[2],"-",fecha_f[1],"-",fecha_f[0]
         emp = array[2],array[3]
         print("Empleado: ",array[2],(""),array[3])
-
         print("Salario Base: ",array[4])
         salario_base = array[4].lower().replace('$', ' ') #Quita los espacios en vacio de la cadena string de la variable creada
         salario_base2 = int(salario_base)
-        bono1 = salario_base2 * BonoGeneral
-        bono_general =  bono1
+        bono1 = salario_base2 * Bono_General
+        bono_general1 =  bono1
         horas_extras = int(array[5])
         print("Horas Extras: ",horas_extras)
         bono_eficiencia = 0
-        print ("Bono General:",bono_general)
+        print ("Bono General:",bono_general1)
 
         if horas_extras > 5: #Bono Eficiencia
-          bono_eficiencia = salario_base2*BonoEficiencia
-          print ("Bono Eficienca:",bono_eficiencia)
+          bono_eficiencia1 = salario_base2*Bono_Eficiencia
+          print ("Bono Eficienca:",bono_eficiencia1)
         else:
-          print ("Bono Eficienca:",bono_eficiencia)
+          print ("Bono Eficienca:",bono_eficiencia1)
 
-        salario = salario_base2 + bono_general + bono_eficiencia
+        salario = salario_base2 + bono_general1 + bono_eficiencia1
         print ("Sueldo:",salario,"$")
         
-      
         archivo2.write(str (cedula) +"***"+ str (fecha_ingreso)+"*********"+ str (emp)+"***********"+ str (salario_base)+"$***************"+ str (horas_extras)+"***************"+ str (salario)+"$\n")
         
-
 archivo.close()
 archivo2.close()
   
+        
+    
         
     
